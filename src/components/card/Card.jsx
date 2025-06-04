a8aa6a58768b3505bb14e3d0c55edde0d07eb09f
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-06-04 13:22:00 -0500

    feat: implement email handling in Card component; enhance EmailField with form submission and validation

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,18 +1,51 @@
+import { useState } from 'react';
 import Illustration from '../illustration/Illustration';
 import EmailField from '../emailField/EmailField';
 import logo from '/images/logo.svg';
-import c from './Card.module.scss'
+import c from './Card.module.scss';
 
 const Card = ({ /*title,*/ paragraph }) => {
+  const [email, setEmail] = useState('');
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email == '') {
+      alert('Please enter an email');
+    } else if (!regex.test(email)) {
+      /* Implementar las toast de SweetAlert2 */
+      alert('please enter a valid email');
+    } else {
+      alert('Correct');
+      setEmail('');
+    }
+  };
+
   return (
     <article className={c.card}>
       <figure className={c['card__logo']}>
-        <img className={c['card__logo-svg']} src={logo} alt="Base Apparel Logo" />
+        <img
+          className={c['card__logo-svg']}
+          src={logo}
+          alt="Base Apparel Logo"
+        />
       </figure>
       <Illustration />
-      <h1 className={c['card__title']}>{/*title*/}<span className={c['card__title-light']}>We're</span> coming soon</h1>
+      <h1 className={c['card__title']}>
+        {/*title*/}
+        <span className={c['card__title-light']}>We're</span> coming soon
+      </h1>
       <p className={c['card__paragraph']}>{paragraph}</p>
-      <EmailField />
+      <EmailField
+        handleEmail={handleEmail}
+        handleSubmit={handleSubmit}
+        value={email}
+      />
     </article>
   );
 };
--- src/components/emailField/EmailField.jsx
+++ src/components/emailField/EmailField.jsx
@@ -1,10 +1,20 @@
 import e from './EmailField.module.scss'
 
-const EmailField = () => {
+const EmailField = ({ handleEmail, value, handleSubmit }) => {
   return (
-    <div className={e.email}>
-
-    </div>
+    <form className={e.email} onSubmit={handleSubmit} noValidate>
+      <input className={e['email__input']}
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email Address"
+        onChange={handleEmail}
+        value={value}
+      />
+      <button className={e['email__button']} type="submit">
+        <img className={e['email__button-svg']} src="images/icon-arrow.svg" alt="Arrow Icon" />
+      </button>
+    </form>
   );
 }
 
